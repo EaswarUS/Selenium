b6b03618afdcf8e0102b49884c9f6661d84ad316
EaswarUS <80040514+EaswarUS@users.noreply.github.com>
2021-03-04 16:52:51 +0530

    Add files via upload

---
+++ Login.java
@@ -0,0 +1,24 @@
+package learnselenium;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Login {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+		driver.get("http://leaftaps.com/opentaps/control/main");
+		driver.findElementById("username").sendKeys("DemoSalesManager");
+		driver.findElementById("password").sendKeys("crmsfa");
+		driver.findElementByClassName("decorativeSubmit").click();
+		
+
+	}
+
+}
