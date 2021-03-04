7ae98d39798e4952abb4e5535d661fbcc0a6294c
EaswarUS <80040514+EaswarUS@users.noreply.github.com>
2021-03-04 16:54:17 +0530

    Add files via upload

---
+++ CreateLead.java
@@ -0,0 +1,31 @@
+package learnselenium;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class CreateLead {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get("http://leaftaps.com/opentaps/control/main");
+		driver.findElementById("username").sendKeys("DemoSalesManager");
+		driver.findElementById("password").sendKeys("crmsfa");
+		driver.findElementByClassName("decorativeSubmit").click();
+		driver.findElementByLinkText("CRM/SFA").click();
+		driver.findElementByLinkText("Leads").click();
+		driver.findElementByLinkText("Create Lead").click();
+		driver.findElementById("createLeadForm_companyName").sendKeys("TestLeaf");
+		driver.findElementById("createLeadForm_firstName").sendKeys("Easwar");
+		driver.findElementById("createLeadForm_lastName").sendKeys("U.S.");
+		driver.findElementById("createLeadForm_primaryPhoneNumber").sendKeys("8680083969");
+		driver.findElementByClassName("smallSubmit").click();
+		
+	}
+
+}
